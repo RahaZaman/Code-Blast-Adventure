0476a2eacfb91df63460154857c8cd22191b2934
Atri Mehta <apmehta@ucsc.edu>
2023-12-03 21:46:44 -0800

    pushing scenes

--- src/scenes/Instruction.js
+++ src/scenes/Instruction.js
@@ -1,14 +1,57 @@
 class Instruction extends Phaser.Scene {
-    constructor() {
-        super('instructionScene');
-    }
-
-    preload() {
-        
+    constructor(){
+        super("instructionScene");
     }
 
     create() {
+        //this.background = this.add.tileSprite(0, 0, 640, 480, 'background').setOrigin(0, 0);
+   
+        let instructionConfig = {
+            fontFamily: 'Merci', 
+            fontSize: '25px', 
+            backgroundColor: 'transparent',
+            color: 'red',
+            align: 'right',
+            padding: {
+                top: 5, 
+                bottom: 5,
+            },
+            fixedWidth: 0
+        };
+
+        let mssgConfig = {
+            fontFamily: 'Merci', 
+            fontSize: '19px', 
+            backgroundColor: 'transparent',
+            color: 'green',
+            align: 'center',
+            padding: {
+                top: 5, 
+                bottom: 5,
+            },
+            fixedWidth: 0
+        };
+        const mssg = `SpaceFootballRun has 2 objectives: Run and Dodge the football coming at you!
+       
+
+        Use the space bar to jump. Good Luck!!
+        `;
+        this.add.text(game.config.width/2, game.config.height/7 - borderUISize - borderPadding, 'Instructions', instructionConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, 250, mssg , mssgConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, 375, 'Click <- to go back to menu' , instructionConfig).setOrigin(0.5);
+
         
+
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        
+   
     }
-      
+    update(){
+
+    
+
+        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+            this.scene.start("menuScene");    
+        }
+    }
 }
